8b8fda5822f2247dc8e8789cc0b09985f52f85ca
Dmytro Kolosovskyi <Dmytro_Kolosovskyi@mac2018.fvt.sk>
2023-03-26 15:01:35 +0200

    Images import modification

---
+++ src/assets/images.js
@@ -0,0 +1,22 @@
+import bodyPart from '../assets/icons/body-part.png';
+import equipment from '../assets/icons/equipment.png';
+import gym from '../assets/icons/gym.png';
+import leftArrow from '../assets/icons/left-arrow.png';
+import rightArrow from '../assets/icons/right-arrow.png';
+import target from '../assets/icons/target.png';
+
+import banner from '../assets/images/banner.png';
+import logo from '../assets/images/Logo.png';
+import logo1 from '../assets/images/Logo-1.png';
+
+export default {
+    bodyPart,
+    equipment,
+    gym,
+    leftArrow,
+    rightArrow,
+    target,
+    banner,
+    logo,
+    logo1
+}
