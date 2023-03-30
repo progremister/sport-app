fd1d452cc89f582874c428c5dd933b166ec29e55
Dmytro Kolosovskyi <Dmytro_Kolosovskyi@mac2018.fvt.sk>
2023-03-30 16:47:12 +0200

    Added the footer template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import {images} from '../assets';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <Box mt="4rem" bgColor="#fff3f4">
+      <Stack gap="4rem" alignItems="center" px="4rem" pt="2.4rem">
+        <img src={images.logo} alt="logo" width="100px" height="100px"/>
+        <Typography variant="h5" pb="4rem" mt="2rem">
+          Made by <a href="" className="referal">Dmytro Kolosovskyi</a>
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
-export default Footer
+export default Footer;
